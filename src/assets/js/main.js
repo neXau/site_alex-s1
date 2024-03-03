487056da062e6354a8a8e05bd7c39eb7bf0dd19a
neXau <c03v23@gmial.com>
2024-03-03 04:21:26 +0200

    Relise

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,3 +2,18 @@ function switchThemeMode() {
     document.documentElement.classList.toggle('dark');
     document.documentElement.classList.toggle('light');
 }
+
+
+const headerMain = document.getElementById('header-main');
+const headerMainMenu = document.getElementById('header-main-menu');
+
+const burgerBtn = document.getElementById('burgerBtn');
+const burgerIcon = document.getElementById('burgerIcon');
+const crossIcon = document.getElementById('crossIcon');
+
+burgerBtn.addEventListener('click', () => {
+    burgerIcon.classList.toggle('hidden');
+    crossIcon.classList.toggle('hidden');
+    headerMainMenu.classList.toggle('opened');
+    headerMain.classList.toggle('menu_opened');
+});
